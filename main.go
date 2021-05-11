8076d0f660d52b64100a705a5cede0c3250a1575
董俊 <418826102@qq.com>
2021-05-11 17:38:29 +0800

    重构 GetRouteVariable

--- main.go
+++ main.go
@@ -114,12 +114,6 @@ type ArticlesFormData struct {
 	Errors      map[string]string
 }
 
-// 通过传参URL路由参数名称获取值
-func getRouteVariable(parameterName string, r *http.Request) string {
-	vars := mux.Vars(r)
-	return vars[parameterName]
-}
-
 // 通过传参id获取博文
 func getArticleByID(id string) (Article, error) {
 	article := Article{}
@@ -130,7 +124,7 @@ func getArticleByID(id string) (Article, error) {
 
 func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 	// 1. 获取URL参数
-	id := getRouteVariable("id", r)
+	id := route.GetRouteVariable("id", r)
 
 	// 2. 读取对应的文章数据
 	article, err := getArticleByID(id)
@@ -304,7 +298,7 @@ func saveArticleToDB(title string, body string) (int64, error) {
 
 func articlesEditHandler(w http.ResponseWriter, r *http.Request) {
 	// 1. 获取URL参数
-	id := getRouteVariable("id", r)
+	id := route.GetRouteVariable("id", r)
 
 	// 2. 读取对应的文章数据
 	article, err := getArticleByID(id)
@@ -340,7 +334,7 @@ func articlesEditHandler(w http.ResponseWriter, r *http.Request) {
 
 func articlesUpdateHandler(w http.ResponseWriter, r *http.Request) {
 	// 1. 获取URL参数
-	id := getRouteVariable("id", r)
+	id := route.GetRouteVariable("id", r)
 
 	// 2. 读取对应的文章数据
 	_, err := getArticleByID(id)
@@ -405,7 +399,7 @@ func articlesUpdateHandler(w http.ResponseWriter, r *http.Request) {
 
 func articlesDeleteHandler(w http.ResponseWriter, r *http.Request) {
 	// 1. 获取url参数
-	id := getRouteVariable("id", r)
+	id := route.GetRouteVariable("id", r)
 
 	// 2. 读取对应的文章数据
 	article, err := getArticleByID(id)
--- pkg/route/route.go
+++ pkg/route/route.go
@@ -1,6 +1,9 @@
 package route
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 // Router 路由对象
 var Router *mux.Router
@@ -18,4 +21,10 @@ func Name2URL(routeName string, paris ...string) string {
 	}
 
 	return url.String()
+}
+
+// 通过传参URL路由参数名称获取值
+func GetRouteVariable(parameterName string, r *http.Request) string {
+	vars := mux.Vars(r)
+	return vars[parameterName]
 }
