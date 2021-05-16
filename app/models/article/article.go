521bceeee380d2220ac19709731990ceeac128ce
董俊 <418826102@qq.com>
2021-05-16 22:48:37 +0800

    文章作者

--- app/models/article/article.go
+++ app/models/article/article.go
@@ -2,6 +2,7 @@ package article
 
 import (
 	"goblog/app/models"
+	"goblog/app/models/user"
 	"goblog/pkg/model"
 	"goblog/pkg/route"
 	"goblog/pkg/types"
@@ -12,13 +13,16 @@ type Article struct {
 	models.BaseModel
 
 	Title string `gorm:"type:varchar(255);not null;" valid:"title"`
-	Body string `gorm:"type:text;not null;" valid:"body"`
+	Body  string `gorm:"type:text;not null;" valid:"body"`
+
+	UserID uint64 `gorm:"not null;index"`
+	User user.User
 }
 
 func Get(idstr string) (Article, error) {
 	var article Article
 	id := types.StringToInt(idstr)
-	if err := model.DB.First(&article, id).Error; err != nil {
+	if err := model.DB.Preload("User").First(&article, id).Error; err != nil {
 		return article, err
 	}
 	return article, nil
@@ -26,7 +30,7 @@ func Get(idstr string) (Article, error) {
 
 func GetAll() ([]Article, error) {
 	var articles []Article
-	if err := model.DB.Find(&articles).Error; err != nil {
+	if err := model.DB.Preload("User").Find(&articles).Error; err != nil {
 		return articles, err
 	}
 	return articles, nil
@@ -34,4 +38,8 @@ func GetAll() ([]Article, error) {
 
 func (a Article) Link() string {
 	return route.Name2URL("articles.show", "id", a.GetStringID())
+}
+
+func (a Article) CreatedAtDate() string {
+	return a.CreatedAt.Format("2006-01-02")
 }
